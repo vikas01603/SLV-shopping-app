5e44b602b53b74c1c26850b0f0c603363a84f2fe
Vikas M <vikasmahadev05@gmail.com>
2026-03-08 19:43:06 +0530

    fixing render deployment issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      "slv-online-stores.onrender.com"
+    ]
+  }
 })
